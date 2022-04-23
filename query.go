20bbdb8c7ba332519d09d476a2d0c6aa381ce5c0
unknown <eceashok1997@gmail.com>
2022-04-24 00:29:54 +0530

    Added function to execeute query

--- query.go
+++ query.go
@@ -6,3 +6,15 @@ func Query(qry string, args ...interface{}) (*sql.Rows, error) {
 	db := GetDB()
 	return db.Query(qry, args...)
 }
+
+func ExecGetID(qry string, args ...interface{}) (uint64, error) {
+	db := GetDB()
+	var err error
+	var res sql.Result
+	if res, err = db.Exec(qry, args...); err != nil {
+		return 0, err
+	}
+	var id int64
+	id, err = res.LastInsertId()
+	return uint64(id), err
+}
